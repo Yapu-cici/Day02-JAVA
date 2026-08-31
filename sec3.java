f76b02f914f995c93620c726f9006c535c24a45a
Yapu-cici <tarhyapu4@gmail.com>
2026-09-01 00:35:01 +0530

    Create sec3.java

---
+++ sec3.java
@@ -0,0 +1,16 @@
+import java.uti1.scanner;
+
+public class main{
+   public static void main(string[] args){
+      scanner sc=new scanner(system.in);
+
+      string[] names=new string[5];
+      system.out.println("enter 5 names:");
+
+  for(int i=0;i<5;i++){
+    names[i]=sc.nextline();
+  }
+     system.out.println("the 5 names are:");
+   }
+  sc.close();
+}
