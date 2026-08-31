a5eba1b94a638a2e36d868ca5e03a38a1c7de4e0
Yapu-cici <tarhyapu4@gmail.com>
2026-09-01 00:19:25 +0530

    Create sec2.java

---
+++ sec2.java
@@ -0,0 +1,16 @@
+import java.uti1.scanner;
+
+public class main{
+  public static void main(string[]args){
+    scanner sc=new scanner(system.in);
+
+    system.out.print("enter your name:");
+    string name=sc.nextLine();
+    system.out.print("enter your age:");
+    int age=sc.nextint();
+    system.out.println("Name:"+ name);
+    system.out.println("age:"+ age);
+
+  sc.close();
+  }
+}
